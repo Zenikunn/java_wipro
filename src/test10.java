194f7be85e59d1d161e609999cce2a10c1562ccf
Zenikunn <vakilgmd@gmail.com>
2025-08-11 21:33:52 +0530

    initial commit

--- src/test10.java
+++ src/test10.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class test10 {
-    
+    public static void main(String[] args) {
+		
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter first number: ");
+	        int a = sc.nextInt();
+	        System.out.print("Enter second number: ");
+	        int b = sc.nextInt();
+	        a = a ^ b;
+	        b = a ^ b;
+	        a = a ^ b;
+	        System.out.println("After swapping: a = " + a + ", b = " + b);
+	        sc.close();
+
+	}
 }
--- src/test13.java
+++ src/test13.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class test13 {
-    
+    public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+	     System.out.print("Enter a number: ");
+	     int a = sc.nextInt();
+	     int result = a >> 1;
+	     System.out.println(a + " / 2 = " + result);
+	     sc.close();
+
+	}
 }
